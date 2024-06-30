9e42797644304353fe7eef4c705854dfe9aa33ef
Brice Ruth <bdruth@gmail.com>
2024-06-30 16:48:27 -0500

    Working initial test IaC for LXC

--- lxc.tf
+++ lxc.tf
@@ -11,7 +11,7 @@ resource "proxmox_lxc" "lxc-test" {
     }
     ostemplate = "f39ac8b4-7319-42ec-b12e-dd3d4d98a85f:vztmpl/debian-12-standard_12.2-1_amd64.tar.zst"
     password = "***REMOVED***"
-    pool = "f39ac8b4-7319-42ec-b12e-dd3d4d98a85f"
+    pool = "terraform-prov-pool"
     target_node = "proxmox-main"
     unprivileged = true
 }
