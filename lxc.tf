659b718f4e7624d7b25f5f88dd952e17134d08c8
Brice Ruth <bruth@flexion.us>
2024-07-04 17:38:12 -0500

    Added storage config to eliminate diff after apply.

--- lxc.tf
+++ lxc.tf
@@ -3,6 +3,7 @@ resource "proxmox_lxc" "lxc-test" {
         nesting = true
     }
     hostname = "terraform-new-container"
+
     network {
         name = "eth0"
         bridge = "vmbr0"
@@ -10,6 +11,17 @@ resource "proxmox_lxc" "lxc-test" {
         gw = "192.168.7.1"
         ip6 = "auto"
     }
+
+    rootfs {
+      acl = false
+      quota = false
+      replicate = false
+      ro = false
+      shared = false
+      storage = ""
+      size = "4G"
+    }
+
     ostemplate = "f39ac8b4-7319-42ec-b12e-dd3d4d98a85f:vztmpl/debian-12-standard_12.2-1_amd64.tar.zst"
     password = "***REMOVED***"
     ssh_public_keys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDDksBMkSWfRLuTAS30E/ZtzY74laZMxa7F6RGyDEJfObQaAyuf1FAmIdTEVgwDJ8gDqtUTy7tDv6reSqGyXNKmEgoNOBdJ4RXPAobZSXX6X2PsgNXd/mrZqHcc/RN4wkJpHmgLoj2ZJuE30Ld0/csP98GeusGREznCEhzuQ3a8N/YVkUtz1lgYKW7yWf5XKD1s/IQBwGmI4eA4I7EkljXtEFIqlEXF4zXNPRpSh+iND++zKdSFZ+ve7ZxWlTlwUCyn5wMRLYNWc+jk84ViX/mQr++dPihC/PVg58UwYK74w1pMfqrrZaHGPuKC64x3x3P1ytblEd752r82iAHTNL8R"
