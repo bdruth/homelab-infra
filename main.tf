d4faa6f81fddcab92510bd75a56777ee6938bb1d
Brice Ruth <bruth@flexion.us>
2024-07-04 17:52:58 -0500

    Move to S3 terraform state

--- main.tf
+++ main.tf
@@ -5,7 +5,25 @@ terraform {
       version = "3.0.1-rc1"
     }
   }
+  backend "s3" {
+    bucket = "homelab-tf-state"                  # Name of the S3 bucket
+    endpoints = {
+        s3 = "http://synology.cusack-ruth.name:9000"   # Minio endpoint
+    }
+    key = "homelab-infra.tfstate"        # Name of the tfstate file
+
+    # access_key=var.access_key          # Access and secret keys
+    # secret_key=var.secret_key
+
+    region = "main"                     # Region validation will be skipped
+    skip_credentials_validation = true  # Skip AWS related checks and validations
+    skip_requesting_account_id = true
+    skip_metadata_api_check = true
+    skip_region_validation = true
+    use_path_style = true             # Enable path-style S3 URLs (https://<HOST>/<BUCKET> https://developer.hashicorp.com/terraform/language/settings/backends/s3#use_path_style
+  }
 }
+
 
 provider "proxmox" {
   # Configuration options
