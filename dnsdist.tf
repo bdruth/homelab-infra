3f61425ca2a4a5688126124b42b183e8ee2ff2ed
Brice Ruth <bruth@flexion.us>
2024-07-05 20:31:36 -0500

    return original name for dnsdist node

--- dnsdist.tf
+++ dnsdist.tf
@@ -1,6 +1,6 @@
 module "dns_ha_lxc" {
   source = "./modules/lxc"
-  lxc_hostname = "dns-ha1"
+  lxc_hostname = "dns-ha"
   lxc_ip_addr = "192.168.7.3"
   lxc_gw_addr = "192.168.7.1"
   lxc_memory = "512"
