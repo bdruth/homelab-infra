7000c1f154ac1ec703ec8aad8947ea6fd27a8f34
Brice Ruth <bruth@flexion.us>
2024-07-05 20:29:03 -0500

    force lxc container to replace if hostname changes

--- dnsdist.tf
+++ dnsdist.tf
@@ -1,10 +1,6 @@
-locals {
-  dns_ha_ip = "192.168.7.3"
-}
-
 module "dns_ha_lxc" {
   source = "./modules/lxc"
-  lxc_hostname = local.dns_ha_ip
+  lxc_hostname = "dns-ha"
   lxc_ip_addr = "192.168.7.3"
   lxc_gw_addr = "192.168.7.1"
   lxc_memory = "512"
