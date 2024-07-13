8e3042a73e79f2d3702eebe6adda7b22530c7d42
Brice Ruth <bruth@flexion.us>
2024-07-13 14:44:32 -0500

    Update automated deploy script to use triple-state rolling deploy - plan only for now.

--- nix-tofu-apply.sh
+++ nix-tofu-apply.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env nix-shell
 #! nix-shell -i bash --pure
 
-tofu init
-tofu apply --auto-approve -compact-warnings
+# shellcheck disable=SC2154
+echo "${ansible_pihole_vars_main_yml}" > ansible/pihole/vars/main.yml
 
+cd dns/pihole || exit
+./tofu-ns.sh blue plan
+./tofu-ns.sh green plan
+
+cd ../dns/dns-ha || exit
+tofu init
+tofu plan
