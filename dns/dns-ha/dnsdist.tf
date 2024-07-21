9693b011350e8d40fc0eff4fd69cbd13d3bbfc56
Brice Ruth <bruth@flexion.us>
2024-07-21 09:33:05 -0500

    try to move new dns-ha state to OG state

--- dns/dns-ha/dnsdist.tf
+++ dns/dns-ha/dnsdist.tf
@@ -1,11 +1,11 @@
-# module "dns_ha_lxc" {
-#   source = "../../modules/lxc"
-#   lxc_hostname = "dns-ha"
-#   lxc_ip_addr = "192.168.7.6"
-#   lxc_gw_addr = var.gw_addr
-#   lxc_memory = "512"
-#   lxc_storage_pool = "local"
-# }
+module "dns_ha_lxc" {
+  source = "../../modules/lxc"
+  lxc_hostname = "dns-ha"
+  lxc_ip_addr = "192.168.7.6"
+  lxc_gw_addr = var.gw_addr
+  lxc_memory = "512"
+  lxc_storage_pool = "local"
+}
 
 locals {
   dns_ip_addrs = [
@@ -14,24 +14,24 @@ locals {
   ]
 }
 
-# resource "null_resource" "install_dnsdist" {
-#   triggers = {
-#     dnsdist_conf_hash    = filesha256("${path.module}/../../ansible/dnsdist/dnsdist.conf.j2")
-#     ansible_conf_hash = filesha256("${path.module}/../../ansible/dnsdist/main.yml")
-#     container_change = module.dns_ha_lxc.lxc_id
-#     dns_ip_addrs = jsonencode(local.dns_ip_addrs)
-#   }
+resource "null_resource" "install_dnsdist" {
+  triggers = {
+    dnsdist_conf_hash    = filesha256("${path.module}/../../ansible/dnsdist/dnsdist.conf.j2")
+    ansible_conf_hash = filesha256("${path.module}/../../ansible/dnsdist/main.yml")
+    container_change = module.dns_ha_lxc.lxc_id
+    dns_ip_addrs = jsonencode(local.dns_ip_addrs)
+  }
 
-#   provisioner "local-exec" {
-#     command     = "until nc -zv ${module.dns_ha_lxc.lxc_ip_addr} 22; do echo 'Waiting for SSH to be available...'; sleep 5; done"
-#     working_dir = path.module
-#   }
-#   provisioner "local-exec" {
-#     command     = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i '${module.dns_ha_lxc.lxc_ip_addr},' -u root --private-key ${var.ssh_priv_key_path} ../../ansible/dnsdist/main.yml -e '{\"dns_ip_addrs\":${jsonencode(local.dns_ip_addrs)}}'"
-#     working_dir = path.module
-#   }
-# }
+  provisioner "local-exec" {
+    command     = "until nc -zv ${module.dns_ha_lxc.lxc_ip_addr} 22; do echo 'Waiting for SSH to be available...'; sleep 5; done"
+    working_dir = path.module
+  }
+  provisioner "local-exec" {
+    command     = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i '${module.dns_ha_lxc.lxc_ip_addr},' -u root --private-key ${var.ssh_priv_key_path} ../../ansible/dnsdist/main.yml -e '{\"dns_ip_addrs\":${jsonencode(local.dns_ip_addrs)}}'"
+    working_dir = path.module
+  }
+}
 
-# output "dns_ha_ip" {
-#   value = module.dns_ha_lxc.lxc_ip_addr
-# }
+output "dns_ha_ip" {
+  value = module.dns_ha_lxc.lxc_ip_addr
+}
