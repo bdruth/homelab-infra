53e5958dccb539965a3e394d40f5ffbbe2bb4f3f
Brice Ruth <bruth@flexion.us>
2024-07-21 16:37:39 -0500

    Update README; remove hard-coding of SSH public key;

--- dns/pihole/pihole.tf
+++ dns/pihole/pihole.tf
@@ -1,13 +1,8 @@
-moved {
-  from = module.pihole-green
-  to   = module.pihole
-}
-
-
 module "pihole" {
   source        = "../../modules/pihole"
   pihole_suffix = var.namespace
   ip_addr       = var.pihole_ip
+  ssh_public_keys = var.ssh_public_keys
 }
 
 output "pihole_ip" {
