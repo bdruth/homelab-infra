694a81d7003f53db1e7404274b5b79c7afe8fca0
Brice Ruth <bruth@flexion.us>
2024-07-20 19:35:29 -0500

    Make LXC storage pool configurable, default to current.

--- modules/lxc/variables.tf
+++ modules/lxc/variables.tf
@@ -24,3 +24,9 @@ variable "ssh_public_keys" {
   type        = string
   default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDDksBMkSWfRLuTAS30E/ZtzY74laZMxa7F6RGyDEJfObQaAyuf1FAmIdTEVgwDJ8gDqtUTy7tDv6reSqGyXNKmEgoNOBdJ4RXPAobZSXX6X2PsgNXd/mrZqHcc/RN4wkJpHmgLoj2ZJuE30Ld0/csP98GeusGREznCEhzuQ3a8N/YVkUtz1lgYKW7yWf5XKD1s/IQBwGmI4eA4I7EkljXtEFIqlEXF4zXNPRpSh+iND++zKdSFZ+ve7ZxWlTlwUCyn5wMRLYNWc+jk84ViX/mQr++dPihC/PVg58UwYK74w1pMfqrrZaHGPuKC64x3x3P1ytblEd752r82iAHTNL8R"
 }
+
+variable "lxc_storage_pool" {
+  description = "LXC storage pool to use for containers"
+  type         = string
+  default      = "fe3b3982-b346-4780-9c84-75bc50c4d81f"
+}
