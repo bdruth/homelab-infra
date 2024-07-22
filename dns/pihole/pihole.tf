1e70ef4233e05ffbc5e548c1f8f3a98e59968ec0
Brice Ruth <bruth@flexion.us>
2024-07-21 21:17:22 -0500

    use the ssh_priv_key_path for ansible key lookup

--- dns/pihole/pihole.tf
+++ dns/pihole/pihole.tf
@@ -3,6 +3,7 @@ module "pihole" {
   pihole_suffix = var.namespace
   ip_addr       = var.pihole_ip
   ssh_public_keys = var.ssh_public_keys
+  ssh_priv_key_path = var.ssh_priv_key_path
 }
 
 output "pihole_ip" {
