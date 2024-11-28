8a117f0af7c55e346f134bf1cee6088f2b4c876b
Brice Ruth <bruth@flexion.us>
2024-11-27 22:53:28 -0600

    Update drone CICD to use `git-crypt`

--- deploy.sh
+++ deploy.sh
@@ -8,20 +8,20 @@ export PATH=/root/.pkgx/ansible.com/v${ANSIBLE_VERSION}/venv/bin:$PATH
 pip install netaddr
 
 
-if [ ! -f ".local" ]; then
-  echo "${ansible_pihole_vars_main_yml}" > ansible/pihole/vars/main.yml
-  echo "${common_tfbackend}" > dns/dns-ha/config.s3.tfbackend
-  echo "${common_tfbackend}" > dns/pihole/blue-config.s3.tfbackend
-  echo "${common_tfbackend}" > dns/pihole/green-config.s3.tfbackend
-  echo "${dnsdist_tfstate_key}" >> dns/dns-ha/config.s3.tfbackend
-  echo "${blue_pihole_tfstate_key}" >> dns/pihole/blue-config.s3.tfbackend
-  echo "${green_pihole_tfstate_key}" >> dns/pihole/green-config.s3.tfbackend
-  echo "${dnsdist_tfvars}" > dns/dns-ha/terraform.tfvars
-  echo "${pihole_blue_tfvars}" > dns/pihole/blue.tfvars
-  echo "${pihole_green_tfvars}" > dns/pihole/green.tfvars
-  echo "${pihole_default_tfvars}" > dns/pihole/terraform.tfvars
-  echo "${dnsdist_ansible_vars}" > ansible/dnsdist/vars/main.yml
-fi
+# if [ ! -f ".local" ]; then
+#   echo "${ansible_pihole_vars_main_yml}" > ansible/pihole/vars/main.yml
+#   echo "${common_tfbackend}" > dns/dns-ha/config.s3.tfbackend
+#   echo "${common_tfbackend}" > dns/pihole/blue-config.s3.tfbackend
+#   echo "${common_tfbackend}" > dns/pihole/green-config.s3.tfbackend
+#   echo "${dnsdist_tfstate_key}" >> dns/dns-ha/config.s3.tfbackend
+#   echo "${blue_pihole_tfstate_key}" >> dns/pihole/blue-config.s3.tfbackend
+#   echo "${green_pihole_tfstate_key}" >> dns/pihole/green-config.s3.tfbackend
+#   echo "${dnsdist_tfvars}" > dns/dns-ha/terraform.tfvars
+#   echo "${pihole_blue_tfvars}" > dns/pihole/blue.tfvars
+#   echo "${pihole_green_tfvars}" > dns/pihole/green.tfvars
+#   echo "${pihole_default_tfvars}" > dns/pihole/terraform.tfvars
+#   echo "${dnsdist_ansible_vars}" > ansible/dnsdist/vars/main.yml
+# fi
 
 tofu --version
 
