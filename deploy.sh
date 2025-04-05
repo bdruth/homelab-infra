29f9c40fa6a0cc1236fd9c478a46c50247d82ffc
Brice Ruth <bruth@flexion.us>
2025-04-05 16:25:49 -0500

    [WIP] NUT UPS monitoring

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,7 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-ansible --version
-ansible-galaxy collection install amazon.aws ansible.utils
-ANSIBLE_VERSION=$(ansible --version | grep core | awk '{print $NF}' | sed 's/]//g')
-export PATH=/root/.pkgx/ansible.com/v${ANSIBLE_VERSION}/venv/bin:$PATH
-pip install netaddr
+"${SCRIPT_DIR}"/setup-ansible.sh
 
 tofu --version
 
