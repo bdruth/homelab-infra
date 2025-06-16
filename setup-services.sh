826554bc0b0e186a141587b10671364995dddf7c
Brice Ruth <bdruth@gmail.com>
2025-06-15 19:39:57 -0500

    Fix SC2034 warning by removing unused SCRIPT_DIR variable

--- setup-services.sh
+++ setup-services.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # shellcheck disable=SC2154
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
 ansible --version
 ansible-galaxy collection install amazon.aws ansible.utils community.general
 ANSIBLE_VERSION=$(ansible --version | grep core | awk '{print $NF}' | sed 's/]//g')
